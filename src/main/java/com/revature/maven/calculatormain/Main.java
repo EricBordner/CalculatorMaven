ac4630d481363d3dfce33a1aee44d776b8a78b97
EricBordner <76772431+EricBordner@users.noreply.github.com>
2022-04-01 15:11:29 -0400

    1st Commit

---
+++ src/main/java/com/revature/maven/calculatormain/Main.java
@@ -0,0 +1,12 @@
+package com.revature.maven.calculatormain;
+
+import com.revature.maven.calculator.Calculator;
+
+public class Main {
+		public static void main(String[] args) {
+		    Calculator c=new Calculator();
+		    
+		    System.out.println("Adding Two Numbers : " +c.add(2, 2));
+		}
+		}
+
